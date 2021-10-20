602b8276074da47225a2c88f746db2d63651a6d4
NicoLorenzo1 <88661584+NicoLorenzo1@users.noreply.github.com>
2021-10-20 10:57:07 -0300

    ejercicio finalizado

--- src/Library/ConsolePrinter.cs
+++ src/Library/ConsolePrinter.cs
@@ -4,9 +4,9 @@ namespace Full_GRASP_And_SOLID
 {
     public class ConsolePrinter : IPrinter
     {
-        public void PrintRecipe(Recipe recipe)
+        public void PrintRecipe(IRecipe result)
         {
-            Console.WriteLine(recipe.GetTextToPrint());
+            Console.WriteLine(result.GetTextToPrint());
         }
     }
 }
